5d99543ddfbad3fb5461293090f8fa56a9580518
Gursimran Singh <g13singh@gmail.com>
2012-01-28 17:51:12 -0600

    Initial Commit

---
+++ basicOpenGL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  basicOpenGL
+//
+//  Created by Gursimran Singh on 1/28/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
